6c9c9e06459821ac492b1e7596275f7d288ac89e
Abhishek Mukhopadhyay <mukho@Sreejitas-MacBook-Pro.local>
2015-02-10 05:24:44 -0800

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Source/Gameplay.m
+++ PeevedPenguins.spritebuilder/Source/Gameplay.m
@@ -8,6 +8,7 @@
 
 #import "Gameplay.h"
 #import "CCPhysics+ObjectiveChipmunk.h"
+#import "Penguin.h"
 
 @implementation Gameplay {
     CCPhysicsNode *_physicsNode;
@@ -17,9 +18,12 @@
     CCNode *_pullbackNode;
     CCNode *_mouseJointNode;
     CCPhysicsJoint *_mouseJoint;
-    CCNode *_currentPenguin;
+    Penguin *_currentPenguin;
     CCPhysicsJoint *_penguinCatapultJoint;
+    CCAction *_followPenguin;
 }
+
+static const float MIN_SPEED = 5.f;
 
 // is called when CCB file has completed loading
 - (void)didLoadFromCCB {
@@ -53,7 +57,7 @@
         _mouseJoint = [CCPhysicsJoint connectedSpringJointWithBodyA:_mouseJointNode.physicsBody bodyB:_catapultArm.physicsBody anchorA:ccp(0, 0) anchorB:ccp(34, 138) restLength:0.f stiffness:3000.f damping:150.f];
         
         // create a penguin from the ccb-file
-        _currentPenguin = [CCBReader load:@"Penguin"];
+        _currentPenguin = (Penguin*)[CCBReader load:@"Penguin"];
         // initially position it on the scoop. 34,138 is the position in the node space of the _catapultArm
         CGPoint penguinPosition = [_catapultArm convertToWorldSpace:ccp(34, 138)];
         // transform the world position to the node space to which the penguin will be added (_physicsNode)
@@ -90,8 +94,10 @@
         _currentPenguin.physicsBody.allowsRotation = TRUE;
         
         // follow the flying penguin
-        CCActionFollow *follow = [CCActionFollow actionWithTarget:_currentPenguin worldBoundary:self.boundingBox];
-        [_contentNode runAction:follow];
+        _followPenguin = [CCActionFollow actionWithTarget:_currentPenguin worldBoundary:self.boundingBox];
+        [_contentNode runAction:_followPenguin];
+        
+        _currentPenguin.launched = TRUE;
     }
 }
 
@@ -106,26 +112,6 @@
     // when touches are cancelled, meaning the user drags their finger off the screen or onto something else, release the catapult
     [self releaseCatapult];
 }
-
-//- (void)launchPenguin {
-//    // loads the Penguin.ccb we have set up in Spritebuilder
-//    CCNode* penguin = [CCBReader load:@"Penguin"];
-//    // position the penguin at the bowl of the catapult
-//    penguin.position = ccpAdd(_catapultArm.position, ccp(16, 50));
-//    
-//    // add the penguin to the physicsNode of this scene (because it has physics enabled)
-//    [_physicsNode addChild:penguin];
-//    
-//    // manually create & apply a force to launch the penguin
-//    CGPoint launchDirection = ccp(1, 0);
-//    CGPoint force = ccpMult(launchDirection, 8000);
-//    [penguin.physicsBody applyForce:force];
-//    
-//    // ensure followed object is in visible are when starting
-//    self.position = ccp(0, 0);
-//    CCActionFollow *follow = [CCActionFollow actionWithTarget:penguin worldBoundary:self.boundingBox];
-//    [_contentNode runAction:follow];
-//}
 
 - (void)retry {
     // reload this level
@@ -155,6 +141,40 @@
             [self sealRemoved:nodeA];
         } key:nodeA];
     }
+}
+
+- (void)update:(CCTime)delta
+{
+    if (_currentPenguin.launched) {
+    
+        // if speed is below minimum speed, assume this attempt is over
+        if (ccpLength(_currentPenguin.physicsBody.velocity) < MIN_SPEED){
+            [self nextAttempt];
+            return;
+        }
+    
+        int xMin = _currentPenguin.boundingBox.origin.x;
+    
+        if (xMin < self.boundingBox.origin.x) {
+            [self nextAttempt];
+            return;
+        }
+    
+        int xMax = xMin + _currentPenguin.boundingBox.size.width;
+    
+        if (xMax > (self.boundingBox.origin.x + self.boundingBox.size.width)) {
+            [self nextAttempt];
+            return;
+        }
+    }
+}
+
+- (void)nextAttempt {
+    _currentPenguin = nil;
+    [_contentNode stopAction:_followPenguin];
+    
+    CCActionMoveTo *actionMoveTo = [CCActionMoveTo actionWithDuration:1.f position:ccp(0, 0)];
+    [_contentNode runAction:actionMoveTo];
 }
 
 @end
