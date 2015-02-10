c4ff8d62e9107570f9d32077a5a299b268d66e0b
Abhishek Mukhopadhyay <mukho@Sreejitas-MacBook-Pro.local>
2015-02-10 01:13:55 -0800

    Automatically Committed for MakeGamesWithUs

---
+++ PeevedPenguins.spritebuilder/Source/Gameplay.h
@@ -0,0 +1,13 @@
+//
+//  Gameplay.h
+//  PeevedPenguins
+//
+//  Created by Abhishek Mukhopadhyay on 2/10/15.
+//  Copyright (c) 2015 Apportable. All rights reserved.
+//
+
+#import "CCNode.h"
+
+@interface Gameplay : CCNode
+
+@end
